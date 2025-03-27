688db0746908b28b050b0ec0b2f915a1da37306c
Sajaldwivedi <127932657+Sajaldwivedi@users.noreply.github.com>
2025-03-27 11:06:34 +0530

    Update App.js

--- cp-visualizer/src/App.js
+++ cp-visualizer/src/App.js
@@ -15,7 +15,7 @@ function App() {
         className="App-header"
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
+        transition={{ duration: 0.8 }} 
       >
         <h1>ProbViz</h1>
         <p>Visualize Competitive Programming Problems</p>
